f1026d4559b204416e4b1decc04cf05fc0c4f1ef
kamrul <kamrul1728@cseku.ac.bd>
2020-06-22 14:06:59 +0600

    Initial Setup

--- App.js
+++ App.js
@@ -4,14 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1,git
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
