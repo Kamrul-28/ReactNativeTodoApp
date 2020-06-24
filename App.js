b2dd3335598c5ebbca694fc23b944a4b572922c6
kamrul <kamrul1728@cseku.ac.bd>
2020-06-24 08:31:14 +0600

    Some work done with useState method

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,{ useState } from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
+
+  const [name,setName]=useState('Kamrul');
+  const [person,setPerson]=useState({ name:'Kamrul Hasan',age:25 });
+
+  const clickHandle=()=>{
+    setName('Hasan')
+    setPerson({
+      name:"Sazid Hasan",
+      age:'23'
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>My name is {name} </Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      
+      <View style={styles.buttonContainer}>
+            <Button title='Click Me' onPress={clickHandle}/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,git
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer:{     
+      marginTop:20
+  }
 });
