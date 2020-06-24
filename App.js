29b16a51fcced68adaa31ff7d51334f142e11121
kamrul <kamrul1728@cseku.ac.bd>
2020-06-24 08:57:51 +0600

    Text Input Handleing

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
-import React ,{ useState } from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
 
-  const [name,setName]=useState('Kamrul');
-  const [person,setPerson]=useState({ name:'Kamrul Hasan',age:25 });
+  const [name, setName] = useState('Kamrul');
+  const [age, setAge] = useState('25');
 
-  const clickHandle=()=>{
-    setName('Hasan')
-    setPerson({
-      name:"Sazid Hasan",
-      age:'23'
-    })
-  }
   return (
     <View style={styles.container}>
-      <Text>My name is {name} </Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      
-      <View style={styles.buttonContainer}>
+      <Text>Enter Your Name</Text>
+      <TextInput   
+          multiline
+          style={styles.input}
+          placeholder='e.g kamrul hasan'
+          onChangeText={(val) => setName(val)}
+
+      />
+      <Text>Enter Your Age</Text>
+      <TextInput
+          // keyboardType='numeric'
+          style={styles.input}
+          placeholder='e.g 25'
+          onChangeText={(val) => setAge(val)}
+
+      />
+
+      <Text>Name: {name} and Age: {age}</Text>
+
+      {/* <View style={styles.buttonContainer}>
             <Button title='Click Me' onPress={clickHandle}/>
-      </View>
+      </View> */}
     </View>
   );
 }
@@ -32,7 +41,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonContainer:{     
-      marginTop:20
+  buttonContainer: {
+    marginTop: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   }
 });
